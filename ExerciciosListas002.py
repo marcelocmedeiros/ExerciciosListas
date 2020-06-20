26d6549f3b96cc4cabec1c16b1d4268b7a8a4f4f
Marcelo Campos de Medeiros <61807949+marcelocmedeiros@users.noreply.github.com>
2020-06-20 15:16:47 -0300

    coloquei no lugar certo lista.sort(reverse = True)

--- ExerciciosListas002.py
+++ ExerciciosListas002.py
@@ -9,13 +9,14 @@
 
 # criando variável lista = função list()
 lista = list()
-# usei função .sort(reverse = True) p colocar na ordem decrescente
-lista.sort(reverse = True)
+
 
 # em um laço for vou digitar os 10 vetores 
 for i in range(1, 11):
     # usei a função append para add cada número a lista
     lista.append(int(input(f'Ditie o {i}° número da lista:')))
+# usei função .sort(reverse = True) p colocar na ordem decrescente
+lista.sort(reverse = True)
 print(lista)
 # pode ser feito com print
 # print(lista[::-1])
