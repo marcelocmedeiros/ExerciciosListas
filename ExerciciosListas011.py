fc22919bd99f35c898e0b2d94f1c64f96430b008
Marcelo Campos de Medeiros <61807949+marcelocmedeiros@users.noreply.github.com>
2020-06-20 16:27:13 -0300

    add

--- ExerciciosListas011.py
+++ ExerciciosListas011.py
@@ -7,22 +7,33 @@
 11 - Altere o programa anterior, intercalando 3 vetores de 10 elementos cada.
 '''
 
+'''
+5 - Faça um Programa que leia 20 números inteiros e armazene-os num vetor.
+Armazene os números pares no vetor PAR e os números IMPARES no vetor impar.
+Imprima os três vetores.
+'''
+print('Primeiro vetor')
+# criei 3 listas p serem impressas no final
 lista1 = []
-for i in range(1, 11):
-    lista1.appendint(input(f'Digite {i}º número: '))
+for i in range(1, 4):
+    lista1.append(int(input(f'Digite {i}º número: ')))
+print('Segundo vetor')
 lista2 = []
-for i in range(1, 11):
-    lista2.appendint(input(f'Digite {i}º número: '))
+for i in range(1, 4):
+    lista2.append(int(input(f'Digite {i}º número: ')))
+print('Terceiro vetor')
 lista3 = []
-for i in range(1, 11):
-    lista2.appendint(input(f'Digite {i}º número: '))
+for i in range(1, 4):
+    lista3.append(int(input(f'Digite {i}º número: ')))
 
 lista4 = []
-# for vai percorrer as duas listas 
-for i in range(0, 10):
+# for vai percorrer as duas listas
+for i in range(0, 3):
     # vai add elementos intercalados dos dois outros vetores
     lista4.append(lista1[i])
     lista4.append(lista2[i])
     lista4.append(lista3[i])
-
+print(lista1)
+print(lista2)
+print(lista3)
 print(lista4)
